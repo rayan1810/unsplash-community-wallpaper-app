1549044c797cd1ee6bf93ff5a9a99d56e252b2ce
Rohit Singh <rohit99.ind@gmail.com>
2021-07-22 00:07:48 +0530

    Started building the app

--- App.tsx
+++ App.tsx
@@ -15,7 +15,9 @@ import {
   Code,
 } from "native-base";
 import NativeBaseIcon from "./components/NativeBaseIcon";
-
+import 'react-native-gesture-handler';
+import {AppBar,Drawer} from './src/components';
+import { NavigationContainer } from '@react-navigation/native';
 // Define the config
 const config = {
   useSystemColorMode: false,
@@ -28,28 +30,10 @@ export const theme = extendTheme({ config });
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Center
-        _dark={{ bg: "blueGray.900" }}
-        _light={{ bg: "blueGray.50" }}
-        px={4}
-        flex={1}
-      >
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Code>App.tsx</Code>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
-          <ToggleDarkMode />
-        </VStack>
-      </Center>
+    <NavigationContainer>
+      <AppBar/>
+      <Drawer/>
+    </NavigationContainer>
     </NativeBaseProvider>
   );
 }
---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export * from './app-bar';
+export {MyDrawer as Drawer} from './drawer'
