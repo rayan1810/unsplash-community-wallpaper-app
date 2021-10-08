0fd277e9f2878e9dac7c073eb1e79fcd4a9aaad7
Rohit Singh <rohit99.ind@gmail.com>
2021-10-09 00:57:51 +0530

    feat: image in masonary layout

--- App.tsx
+++ App.tsx
@@ -9,6 +9,9 @@ import {
   Square,
   Box,
   ScrollView,
+  StatusBar,
+  AspectRatio,
+  Image,
 } from "native-base";
 import "react-native-gesture-handler";
 import { AppBar, Drawer, MasonaryLayout } from "./src/components";
@@ -23,9 +26,7 @@ const config = {
 export const theme = extendTheme({ config });
 const CustomComp = ({ minH, props }: any) => {
   return (
-    <Square bg="primary.400"
-    minH={minH}
-    >
+    <Square bg="primary.400" minH={minH}>
       <Box
         _text={{
           fontWeight: "bold",
@@ -34,12 +35,256 @@ const CustomComp = ({ minH, props }: any) => {
         }}
         {...props}
       >
-        20
+        Test
       </Box>
     </Square>
   );
 };
 
+// Server API that returns Images details
+const imgData = [
+  {
+    url: "https://iasdas.com/photo-1508739773434-c26b3d09e071?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Brow Rock",
+    owner: "Cristina Gottardi",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Blue Orange Smoke",
+    owner: "Lucas Benjamin",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1481833761820-0509d3217039?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Restaurant Relax",
+    owner: "Clem Onojeghuo",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1453791052107-5c843da62d97?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Tree of life",
+    owner: "Veeterzy",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Reflection",
+    owner: "Garrett parker",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1479030160180-b1860951d696?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Canyon Pass",
+    owner: "Ashim D’Silva",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1508739773434-c26b3d09e071?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Brow Rock",
+    owner: "Cristina Gottardi",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Blue Orange Smoke",
+    owner: "Lucas Benjamin",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1481833761820-0509d3217039?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Restaurant Relax",
+    owner: "Clem Onojeghuo",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1453791052107-5c843da62d97?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Tree of life",
+    owner: "Veeterzy",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Reflection",
+    owner: "Garrett parker",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1479030160180-b1860951d696?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Canyon Pass",
+    owner: "Ashim D’Silva",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1508739773434-c26b3d09e071?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Brow Rock",
+    owner: "Cristina Gottardi",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Blue Orange Smoke",
+    owner: "Lucas Benjamin",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1481833761820-0509d3217039?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Restaurant Relax",
+    owner: "Clem Onojeghuo",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1453791052107-5c843da62d97?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Tree of life",
+    owner: "Veeterzy",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Reflection",
+    owner: "Garrett parker",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1479030160180-b1860951d696?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Canyon Pass",
+    owner: "Ashim D’Silva",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1508739773434-c26b3d09e071?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Brow Rock",
+    owner: "Cristina Gottardi",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Blue Orange Smoke",
+    owner: "Lucas Benjamin",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1481833761820-0509d3217039?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Restaurant Relax",
+    owner: "Clem Onojeghuo",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1453791052107-5c843da62d97?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Tree of life",
+    owner: "Veeterzy",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Reflection",
+    owner: "Garrett parker",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1479030160180-b1860951d696?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Canyon Pass",
+    owner: "Ashim D’Silva",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1508739773434-c26b3d09e071?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Brow Rock",
+    owner: "Cristina Gottardi",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Blue Orange Smoke",
+    owner: "Lucas Benjamin",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1481833761820-0509d3217039?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Restaurant Relax",
+    owner: "Clem Onojeghuo",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1453791052107-5c843da62d97?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Tree of life",
+    owner: "Veeterzy",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Reflection",
+    owner: "Garrett parker",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1479030160180-b1860951d696?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Canyon Pass",
+    owner: "Ashim D’Silva",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1508739773434-c26b3d09e071?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Brow Rock",
+    owner: "Cristina Gottardi",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Blue Orange Smoke",
+    owner: "Lucas Benjamin",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1481833761820-0509d3217039?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Restaurant Relax",
+    owner: "Clem Onojeghuo",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1453791052107-5c843da62d97?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Tree of life",
+    owner: "Veeterzy",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Reflection",
+    owner: "Garrett parker",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1479030160180-b1860951d696?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Canyon Pass",
+    owner: "Ashim D’Silva",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1508739773434-c26b3d09e071?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Brow Rock",
+    owner: "Cristina Gottardi",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Blue Orange Smoke",
+    owner: "Lucas Benjamin",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1481833761820-0509d3217039?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Restaurant Relax",
+    owner: "Clem Onojeghuo",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1453791052107-5c843da62d97?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Tree of life",
+    owner: "Veeterzy",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Reflection",
+    owner: "Garrett parker",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1479030160180-b1860951d696?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Canyon Pass",
+    owner: "Ashim D’Silva",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1508739773434-c26b3d09e071?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Brow Rock",
+    owner: "Cristina Gottardi",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Blue Orange Smoke",
+    owner: "Lucas Benjamin",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1481833761820-0509d3217039?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Restaurant Relax",
+    owner: "Clem Onojeghuo",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1453791052107-5c843da62d97?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Tree of life",
+    owner: "Veeterzy",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Reflection",
+    owner: "Garrett parker",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1479030160180-b1860951d696?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    name: "Canyon Pass",
+    owner: "Ashim D’Silva",
+  },
+];
+const MasonaryLayoutItemHeights = [32, 40, 48, 48, 40];
 export default function App() {
   return (
     <NativeBaseProvider>
@@ -59,19 +304,18 @@ export default function App() {
             }}
             _vStack={{ space: 4 }}
           >
-            <CustomComp minH={32} />
-            <CustomComp minH={40} />
-            <CustomComp minH={48} />
-            <CustomComp minH={48} />
-            <CustomComp minH={32}/>
-            <CustomComp minH={40}/>
-            <CustomComp minH={40}/>
-            <CustomComp minH={40}/>
-            <CustomComp minH={32}/>
-            <CustomComp minH={32}/>
-            <CustomComp minH={40}/>
-            <CustomComp minH={40}/>
-            <CustomComp minH={24}/>
+            {imgData.map((img, ind) => {
+              return (
+                <Image
+                  fallbackSource={{
+                    uri: "https://images.unsplash.com/photo-1578328819058-b69f3a3b0f6b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80",
+                  }}
+                  source={{ uri: img.url }}
+                  h={MasonaryLayoutItemHeights[ind % 5]}
+                  alt={img.name}
+                />
+              );
+            })}
           </MasonaryLayout>
         </ScrollView>
       </NavigationContainer>
--- src/components/app-bar.tsx
+++ src/components/app-bar.tsx
@@ -1,27 +1,76 @@
 import React from "react";
-import { VStack, HStack, Button, IconButton, Icon, Text, NativeBaseProvider, Center, Box, StatusBar } from "native-base";
-import { MaterialIcons } from '@expo/vector-icons';
+import {
+  VStack,
+  HStack,
+  Button,
+  IconButton,
+  Icon,
+  Text,
+  NativeBaseProvider,
+  Center,
+  Box,
+  StatusBar,
+} from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
 
-export function AppBar(){
+export function AppBar() {
   return (
     <>
-        <StatusBar backgroundColor="blueGray.900" barStyle="light-content" />
+      <StatusBar backgroundColor="blueGray.900" barStyle="light-content" />
 
-        <Box safeAreaTop backgroundColor="blueGray.900" />
+      <Box safeAreaTop backgroundColor="blueGray.900" />
 
-        <HStack bg='blueGray.900' px={1} py={3} justifyContent='space-between' alignItems='center'>
-          <HStack space={4} alignItems='center'>
-            <IconButton icon={<Icon size="sm" as={<MaterialIcons name='menu' />} color="white" />} />
-            <Text color="white" fontSize={20} fontWeight='bold'>Home</Text>
-          </HStack>
-          <HStack space={2}>
-            <IconButton icon={<Icon as={<MaterialIcons name='favorite' />} size='sm' color="white" />} />
-            <IconButton icon={<Icon as={<MaterialIcons name='search' />}
-            color="white" size='sm'  />} />
-            <IconButton icon={<Icon as={<MaterialIcons name='more-vert' />} size='sm' color="white" />} />
-          </HStack>
+      <HStack
+        bg="blueGray.900"
+        px={1}
+        py={3}
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <HStack space={4} alignItems="center">
+          <IconButton
+            icon={
+              <Icon
+                size="sm"
+                as={<MaterialIcons name="menu" />}
+                color="white"
+              />
+            }
+          />
+          <Text color="white" fontSize={20} fontWeight="bold">
+            Home
+          </Text>
         </HStack>
-
+        <HStack space={2}>
+          <IconButton
+            icon={
+              <Icon
+                as={<MaterialIcons name="favorite" />}
+                size="sm"
+                color="white"
+              />
+            }
+          />
+          <IconButton
+            icon={
+              <Icon
+                as={<MaterialIcons name="search" />}
+                color="white"
+                size="sm"
+              />
+            }
+          />
+          <IconButton
+            icon={
+              <Icon
+                as={<MaterialIcons name="more-vert" />}
+                size="sm"
+                color="white"
+              />
+            }
+          />
+        </HStack>
+      </HStack>
     </>
-  )
+  );
 }
